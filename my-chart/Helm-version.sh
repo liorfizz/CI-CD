1e5a421dcb9490f769970efdef97beb5b99cd420
liorfizz <134203374+liorfizz@users.noreply.github.com>
2023-08-16 12:12:12 +0300

    Update Helm-version.sh

--- my-chart/Helm-version.sh
+++ my-chart/Helm-version.sh
@@ -11,6 +11,7 @@ CURRENT_VERSION=$(grep '^version:' "${CHART_YAML}" | awk '{print $2}')
 NEW_VERSION=$((CURRENT_VERSION + 1))
 
 # Update the version in Chart.yaml
-sed -i "s/^version: ${CURRENT_VERSION}/version: ${NEW_VERSION}/" "${CHART_YAML}"
+sed -i.bak "s/^version: ${CURRENT_VERSION}/version: ${NEW_VERSION}/" "${CHART_YAML}"
+rm "${CHART_YAML}.bak"
 
 echo "Updated version in Chart.yaml from ${CURRENT_VERSION} to ${NEW_VERSION}"
