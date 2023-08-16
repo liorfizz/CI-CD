e0ad263d75b97498f6f3f4d09cf37464fe5e1956
liorfizz <134203374+liorfizz@users.noreply.github.com>
2023-08-16 14:12:13 +0300

    Update Helm-version.sh

--- my-chart/Helm-version.sh
+++ my-chart/Helm-version.sh
@@ -4,21 +4,21 @@
 CHART_DIR="."
 CHART_YAML="${CHART_DIR}/Chart.yaml"
 
-# Extract the current version from Chart.yaml
-CURRENT_VERSION=$(grep '^version:' "${CHART_YAML}" | awk '{print $2}')
+# Retrieve the latest version from Google Cloud Storage
+LATEST_VERSION=$(gsutil ls gs://helmflask-bucket/ | grep -oP 'my-chart-\K\d+\.\d+\.\d+' | sort -Vr | head -n 1)
 
 # Convert version components to integers
-MAJOR=$(echo "${CURRENT_VERSION}" | cut -d. -f1)
-MINOR=$(echo "${CURRENT_VERSION}" | cut -d. -f2)
-PATCH=$(echo "${CURRENT_VERSION}" | cut -d. -f3)
+MAJOR=$(echo "${LATEST_VERSION}" | cut -d. -f1)
+MINOR=$(echo "${LATEST_VERSION}" | cut -d. -f2)
+PATCH=$(echo "${LATEST_VERSION}" | cut -d. -f3)
 
 # Increment the version (0.0.1 style)
 PATCH=$((PATCH + 1))
 
 # Update the version in Chart.yaml
 NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
-sed -i.bak "s/^version: ${CURRENT_VERSION}/version: ${NEW_VERSION}/" "${CHART_YAML}"
+sed -i.bak "s/^version: ${LATEST_VERSION}/version: ${NEW_VERSION}/" "${CHART_YAML}"
 rm "${CHART_YAML}.bak"
 
-echo "Updated version in Chart.yaml from ${CURRENT_VERSION} to ${NEW_VERSION}"
-
+echo "Latest version in Google Cloud Storage: ${LATEST_VERSION}"
+echo "Updated version in Chart.yaml to ${NEW_VERSION}"
