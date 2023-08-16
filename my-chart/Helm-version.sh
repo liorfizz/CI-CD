1be82b9f5eee55c2b209d3b477a383c16f888359
liorfizz <134203374+liorfizz@users.noreply.github.com>
2023-08-16 14:16:23 +0300

    Update Helm-version.sh

--- my-chart/Helm-version.sh
+++ my-chart/Helm-version.sh
@@ -20,5 +20,10 @@ NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
 sed -i.bak "s/^version: ${LATEST_VERSION}/version: ${NEW_VERSION}/" "${CHART_YAML}"
 rm "${CHART_YAML}.bak"
 
+# Navigate to chart directory, package the chart, and upload to Google Cloud Storage
+cd ci-cd/my-chart/ && helm package .
+gsutil cp my-chart-${NEW_VERSION}.tgz gs://helmflask-bucket/
+
 echo "Latest version in Google Cloud Storage: ${LATEST_VERSION}"
 echo "Updated version in Chart.yaml to ${NEW_VERSION}"
+echo "Packaged chart and uploaded to Google Cloud Storage"
