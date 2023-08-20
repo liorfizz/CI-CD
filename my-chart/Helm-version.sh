2f7d14156ca450ea5b3ea0c1fee4f43295a6da53
liorfizz <134203374+liorfizz@users.noreply.github.com>
2023-08-20 13:21:12 +0300

    Update Helm-version.sh

--- my-chart/Helm-version.sh
+++ my-chart/Helm-version.sh
@@ -3,7 +3,6 @@
 # Define the chart directory and Chart.yaml path
 CHART_DIR="."
 CHART_YAML="${CHART_DIR}/Chart.yaml"
-PWD=$(pwd)
 
 # Retrieve the latest version from Google Cloud Storage
 LATEST_VERSION=$(gsutil ls gs://helmflask-bucket/ | grep -oP 'my-chart-\K\d+\.\d+\.\d+' | sort -Vr | head -n 1)
@@ -18,7 +17,7 @@ PATCH=$((PATCH + 1))
 
 # Update the version in Chart.yaml
 NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
-sed -i.bak "s/^version: */version: ${NEW_VERSION}/" "${CHART_YAML}"
+sed -i.bak "s/^version: 0.1.0/version: ${NEW_VERSION}/" "${CHART_YAML}"
 rm "${CHART_YAML}.bak"
 
 # Navigate to chart directory, package the chart, and upload to Google Cloud Storage
@@ -26,7 +25,6 @@ helm package .  # Package the Helm chart
 gsutil cp ./my-chart-${NEW_VERSION}.tgz gs://helmflask-bucket/  # Copy the chart package to Google Cloud Storage
 
 # Display messages about the process
-echo "You are in: ${PWD}"
 echo "Latest version in Google Cloud Storage: ${LATEST_VERSION}"
 echo "Updated version in Chart.yaml to ${NEW_VERSION}"
 echo "Packaged chart and uploaded to Google Cloud Storage"
